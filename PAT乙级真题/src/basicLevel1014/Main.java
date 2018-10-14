8669f18d4201953d62ed13981a214b7283b308ff
abdullah199694 <826479138@qq.com>
2018-10-14 17:46:25 +0800

    String

---
+++ PAT乙级真题/src/basicLevel1014/Main.java
@@ -0,0 +1,20 @@
+package basicLevel1014;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		
+		Scanner input = new Scanner(System.in);
+		
+		String str1 = input.nextLine();
+		String str2 = input.nextLine();
+		String str3 = input.nextLine();
+		String str4 = input.nextLine();
+		
+		
+	}
+
+}
